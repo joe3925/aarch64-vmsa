2f764a5cdcb7a370fc5342c41e4f8a844981b0a1
boden1203 <bodenboden212@gmail.com>
2026-07-14 01:54:58 -0700

    model privilege limits and physical address spaces

--- attrs/pas.rs
+++ attrs/pas.rs
@@ -1,116 +1,135 @@
 use core::fmt::Debug;
 
-use crate::arch::VmsaFeatures;
-use crate::translation::{IpaSpace, TranslationSpace};
+use crate::arch::{FeatureRequirements, SecurityStates};
 
-#[derive(Clone, Copy, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
-pub enum OutputAddressSpace {
-    NonSecure,
-    Secure,
-    Realm,
-    Root,
+pub trait PasModel: Copy + 'static {
+    const REQUIRED_FEATURES: FeatureRequirements;
 }
 
-pub trait Stage1PasModel: Copy + 'static {
+pub trait Stage1PasModel: PasModel {
     type LeafAttr: Copy + Debug + Eq + PartialEq;
     type TableAttr: Copy + Debug + Eq + PartialEq;
-
-    const SPACE: TranslationSpace;
-    const REQUIRED_FEATURES: VmsaFeatures;
 }
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct FixedNonSecurePas;
-
+impl PasModel for FixedNonSecurePas {
+    const REQUIRED_FEATURES: FeatureRequirements = FeatureRequirements::NONE;
+}
 impl Stage1PasModel for FixedNonSecurePas {
     type LeafAttr = ();
     type TableAttr = ();
+}
 
-    const SPACE: TranslationSpace = TranslationSpace::NonSecure;
-    const REQUIRED_FEATURES: VmsaFeatures = VmsaFeatures::NONE;
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub enum SecureSelectablePa {
+    Secure,
+    NonSecure,
 }
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct SecureSelectablePas;
-
+impl PasModel for SecureSelectablePas {
+    const REQUIRED_FEATURES: FeatureRequirements =
+        FeatureRequirements::NONE.with_security_state(SecurityStates::SECURE);
+}
 impl Stage1PasModel for SecureSelectablePas {
-    type LeafAttr = OutputAddressSpace;
-    type TableAttr = OutputAddressSpace;
-
-    const SPACE: TranslationSpace = TranslationSpace::Secure;
-    const REQUIRED_FEATURES: VmsaFeatures = VmsaFeatures::NONE.with_secure_state();
+    type LeafAttr = SecureSelectablePa;
+    type TableAttr = SecureSelectablePa;
 }
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
-pub struct RealmPas;
-
-impl Stage1PasModel for RealmPas {
-    type LeafAttr = OutputAddressSpace;
-    type TableAttr = OutputAddressSpace;
-
-    const SPACE: TranslationSpace = TranslationSpace::Realm;
-    const REQUIRED_FEATURES: VmsaFeatures = VmsaFeatures::NONE.with_rme();
+pub struct FixedRealmIpaPas;
+impl PasModel for FixedRealmIpaPas {
+    const REQUIRED_FEATURES: FeatureRequirements = FeatureRequirements::NONE
+        .with_rme()
+        .with_security_state(SecurityStates::REALM);
+}
+impl Stage1PasModel for FixedRealmIpaPas {
+    type LeafAttr = ();
+    type TableAttr = ();
 }
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
-pub struct RootPas;
-
-impl Stage1PasModel for RootPas {
-    type LeafAttr = OutputAddressSpace;
-    type TableAttr = OutputAddressSpace;
-
-    const SPACE: TranslationSpace = TranslationSpace::Root;
-    const REQUIRED_FEATURES: VmsaFeatures = VmsaFeatures::NONE.with_rme();
+pub enum RealmOrNonSecurePa {
+    Realm,
+    NonSecure,
 }
 
-pub trait Stage2PasContext: Copy + 'static {
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub struct RealmOrNonSecurePaPas;
+impl PasModel for RealmOrNonSecurePaPas {
+    const REQUIRED_FEATURES: FeatureRequirements = FeatureRequirements::NONE
+        .with_rme()
+        .with_security_state(SecurityStates::REALM);
+}
+impl Stage1PasModel for RealmOrNonSecurePaPas {
+    type LeafAttr = RealmOrNonSecurePa;
+    type TableAttr = ();
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub enum RootExtendedPa {
+    Secure,
+    NonSecure,
+    Root,
+    Realm,
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub struct RootExtendedPas;
+impl PasModel for RootExtendedPas {
+    const REQUIRED_FEATURES: FeatureRequirements = FeatureRequirements::NONE
+        .with_rme()
+        .with_el3()
+        .with_security_state(SecurityStates::ROOT);
+}
+impl Stage1PasModel for RootExtendedPas {
+    type LeafAttr = RootExtendedPa;
+    type TableAttr = ();
+}
+
+pub trait Stage2PasContext: PasModel {
     type OutputAddressSpaceAttr: Copy + Debug + Eq + PartialEq;
-
-    const SPACE: TranslationSpace;
-    const IPA_SPACE: IpaSpace;
-    const REQUIRED_FEATURES: VmsaFeatures;
 }
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct NonSecureIpaContext;
-
+impl PasModel for NonSecureIpaContext {
+    const REQUIRED_FEATURES: FeatureRequirements = FeatureRequirements::NONE;
+}
 impl Stage2PasContext for NonSecureIpaContext {
     type OutputAddressSpaceAttr = ();
-
-    const SPACE: TranslationSpace = TranslationSpace::NonSecure;
-    const IPA_SPACE: IpaSpace = IpaSpace::NonSecure;
-    const REQUIRED_FEATURES: VmsaFeatures = VmsaFeatures::NONE;
 }
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct SecureIpaContext;
-
+impl PasModel for SecureIpaContext {
+    const REQUIRED_FEATURES: FeatureRequirements = FeatureRequirements::NONE
+        .with_el2()
+        .with_sel2()
+        .with_security_state(SecurityStates::SECURE);
+}
 impl Stage2PasContext for SecureIpaContext {
-    type OutputAddressSpaceAttr = OutputAddressSpace;
-
-    const SPACE: TranslationSpace = TranslationSpace::Secure;
-    const IPA_SPACE: IpaSpace = IpaSpace::Secure;
-    const REQUIRED_FEATURES: VmsaFeatures = VmsaFeatures::NONE.with_secure_state().with_sel2();
+    type OutputAddressSpaceAttr = SecureSelectablePa;
 }
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct SecureNonSecureIpaContext;
-
+impl PasModel for SecureNonSecureIpaContext {
+    const REQUIRED_FEATURES: FeatureRequirements = SecureIpaContext::REQUIRED_FEATURES;
+}
 impl Stage2PasContext for SecureNonSecureIpaContext {
-    type OutputAddressSpaceAttr = OutputAddressSpace;
-
-    const SPACE: TranslationSpace = TranslationSpace::Secure;
-    const IPA_SPACE: IpaSpace = IpaSpace::NonSecure;
-    const REQUIRED_FEATURES: VmsaFeatures = VmsaFeatures::NONE.with_secure_state().with_sel2();
+    type OutputAddressSpaceAttr = SecureSelectablePa;
 }
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct RealmIpaContext;
-
+impl PasModel for RealmIpaContext {
+    const REQUIRED_FEATURES: FeatureRequirements = FeatureRequirements::NONE
+        .with_rme()
+        .with_security_state(SecurityStates::REALM);
+}
 impl Stage2PasContext for RealmIpaContext {
-    type OutputAddressSpaceAttr = OutputAddressSpace;
-
-    const SPACE: TranslationSpace = TranslationSpace::Realm;
-    const IPA_SPACE: IpaSpace = IpaSpace::Realm;
-    const REQUIRED_FEATURES: VmsaFeatures = VmsaFeatures::NONE.with_rme();
+    type OutputAddressSpaceAttr = RealmOrNonSecurePa;
 }
--- attrs/permissions.rs
+++ attrs/permissions.rs
@@ -1,7 +1,6 @@
 use core::fmt::Debug;
 
-use crate::arch::VmsaFeatures;
-use crate::translation::RegimeOwner;
+use crate::arch::FeatureRequirements;
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
 pub enum DataAccess {
@@ -17,9 +16,9 @@ pub struct SinglePrivilegeLeafPermissions {
 }
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
-pub struct SinglePrivilegeTablePermissions {
+pub struct SinglePrivilegeTablePermissionLimits {
     pub data_limit: DataAccess,
-    pub execute: bool,
+    pub execute_limit: bool,
 }
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
@@ -31,11 +30,11 @@ pub struct TwoPrivilegeLeafPermissions {
 }
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
-pub struct TwoPrivilegeTablePermissions {
+pub struct TwoPrivilegeTablePermissionLimits {
     pub privileged_data_limit: DataAccess,
     pub unprivileged_data_limit: DataAccess,
-    pub privileged_execute: bool,
-    pub unprivileged_execute: bool,
+    pub privileged_execute_limit: bool,
+    pub unprivileged_execute_limit: bool,
 }
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
@@ -45,106 +44,109 @@ pub struct Stage2LeafPermissions {
     pub unprivileged_execute: bool,
 }
 
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
-pub struct Stage2TablePermissions {
-    pub data_limit: DataAccess,
-    pub privileged_execute: bool,
-    pub unprivileged_execute: bool,
-}
-
-pub trait PermissionModel: Copy + 'static {
+pub trait PrivilegeModel: Copy + 'static {
     type LeafPermissions: Copy + Debug + Eq + PartialEq;
-    type TablePermissions: Copy + Debug + Eq + PartialEq;
-
-    const OWNER: RegimeOwner;
+    type TablePermissionLimits: Copy + Debug + Eq + PartialEq;
     const SUPPORTS_EL0: bool;
     const HAS_TTBR1: bool;
-    const REQUIRED_FEATURES: VmsaFeatures;
-}
-
-pub trait Stage2PermissionModel:
-    PermissionModel<LeafPermissions = Stage2LeafPermissions, TablePermissions = Stage2TablePermissions>
-{
+    const REQUIRED_FEATURES: FeatureRequirements;
 }
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct El1And0Permissions;
-
-impl PermissionModel for El1And0Permissions {
+impl PrivilegeModel for El1And0Permissions {
     type LeafPermissions = TwoPrivilegeLeafPermissions;
-    type TablePermissions = TwoPrivilegeTablePermissions;
-
-    const OWNER: RegimeOwner = RegimeOwner::El1;
+    type TablePermissionLimits = TwoPrivilegeTablePermissionLimits;
     const SUPPORTS_EL0: bool = true;
     const HAS_TTBR1: bool = true;
-    const REQUIRED_FEATURES: VmsaFeatures = VmsaFeatures::NONE;
+    const REQUIRED_FEATURES: FeatureRequirements = FeatureRequirements::NONE;
 }
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct El2Permissions;
-
-impl PermissionModel for El2Permissions {
+impl PrivilegeModel for El2Permissions {
     type LeafPermissions = SinglePrivilegeLeafPermissions;
-    type TablePermissions = SinglePrivilegeTablePermissions;
-
-    const OWNER: RegimeOwner = RegimeOwner::El2;
+    type TablePermissionLimits = SinglePrivilegeTablePermissionLimits;
     const SUPPORTS_EL0: bool = false;
     const HAS_TTBR1: bool = false;
-    const REQUIRED_FEATURES: VmsaFeatures = VmsaFeatures::NONE.with_el2();
+    const REQUIRED_FEATURES: FeatureRequirements = FeatureRequirements::NONE.with_el2();
 }
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct El2And0Permissions;
-
-impl PermissionModel for El2And0Permissions {
+impl PrivilegeModel for El2And0Permissions {
     type LeafPermissions = TwoPrivilegeLeafPermissions;
-    type TablePermissions = TwoPrivilegeTablePermissions;
-
-    const OWNER: RegimeOwner = RegimeOwner::El2;
+    type TablePermissionLimits = TwoPrivilegeTablePermissionLimits;
     const SUPPORTS_EL0: bool = true;
     const HAS_TTBR1: bool = true;
-    const REQUIRED_FEATURES: VmsaFeatures = VmsaFeatures::NONE.with_el2().with_el2_and0();
+    const REQUIRED_FEATURES: FeatureRequirements =
+        FeatureRequirements::NONE.with_el2().with_el2_and0();
 }
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct El3Permissions;
-
-impl PermissionModel for El3Permissions {
+impl PrivilegeModel for El3Permissions {
     type LeafPermissions = SinglePrivilegeLeafPermissions;
-    type TablePermissions = SinglePrivilegeTablePermissions;
-
-    const OWNER: RegimeOwner = RegimeOwner::El3;
+    type TablePermissionLimits = SinglePrivilegeTablePermissionLimits;
     const SUPPORTS_EL0: bool = false;
     const HAS_TTBR1: bool = false;
-    const REQUIRED_FEATURES: VmsaFeatures = VmsaFeatures::NONE.with_el3();
+    const REQUIRED_FEATURES: FeatureRequirements = FeatureRequirements::NONE.with_el3();
 }
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct Stage2Permissions;
 
-impl PermissionModel for Stage2Permissions {
-    type LeafPermissions = Stage2LeafPermissions;
-    type TablePermissions = Stage2TablePermissions;
-
-    const OWNER: RegimeOwner = RegimeOwner::El2;
-    const SUPPORTS_EL0: bool = false;
-    const HAS_TTBR1: bool = false;
-    const REQUIRED_FEATURES: VmsaFeatures = VmsaFeatures::NONE.with_el2().with_stage2();
-}
-
-impl Stage2PermissionModel for Stage2Permissions {}
-
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct Stage2XnxPermissions;
 
-impl PermissionModel for Stage2XnxPermissions {
-    type LeafPermissions = Stage2LeafPermissions;
-    type TablePermissions = Stage2TablePermissions;
-
-    const OWNER: RegimeOwner = RegimeOwner::El2;
-    const SUPPORTS_EL0: bool = false;
-    const HAS_TTBR1: bool = false;
-    const REQUIRED_FEATURES: VmsaFeatures = VmsaFeatures::NONE.with_el2().with_stage2().with_xnx();
+pub trait Stage2PermissionModel: Copy + 'static {
+    const REQUIRED_FEATURES: FeatureRequirements;
+    const XNX: bool;
 }
 
-impl Stage2PermissionModel for Stage2XnxPermissions {}
+impl Stage2PermissionModel for Stage2Permissions {
+    const REQUIRED_FEATURES: FeatureRequirements =
+        FeatureRequirements::NONE.with_el2().with_stage2();
+    const XNX: bool = false;
+}
+
+impl Stage2PermissionModel for Stage2XnxPermissions {
+    const REQUIRED_FEATURES: FeatureRequirements = FeatureRequirements::NONE
+        .with_el2()
+        .with_stage2()
+        .with_xnx();
+    const XNX: bool = true;
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub struct Stage1EffectivePermissions {
+    pub privileged_data: DataAccess,
+    pub unprivileged_data: DataAccess,
+    pub privileged_execute: bool,
+    pub unprivileged_execute: bool,
+    pub privileged_gcs: bool,
+    pub unprivileged_gcs: bool,
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
+pub enum MostlyReadOnly {
+    Unqualified,
+    TopLevel1,
+    TopLevel0,
+    TopLevels0And1,
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
+pub enum Stage2Permission {
+    NoAccess,
+    MostlyReadOnly(MostlyReadOnly),
+    WriteOnly,
+    ReadOnly {
+        privileged_execute: bool,
+        unprivileged_execute: bool,
+    },
+    ReadWrite {
+        privileged_execute: bool,
+        unprivileged_execute: bool,
+    },
+}
